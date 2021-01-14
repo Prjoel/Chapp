fd5a35016b56013455d787fe4fcbd8830a35190b
Joel Pereira <joel.pereira.996@gmail.com>
2021-01-14 15:32:58 -0300

    adding Users model & migration

---
+++ backend/dbservice/userService.js
@@ -0,0 +1,56 @@
+const { User } = require('../db/models');
+
+class UserService {
+
+  static async saveUser(user) {
+    try {
+      await User.create(user);
+      console.log('User added!')
+    } catch (e) {
+      console.log('Oh no! something really bad happened x0 saveuser(user)', e)
+    }
+  }
+
+  static async getUserEmail(userEmail) {
+    try {
+      let user = await User.findOne({
+        where: {
+          email: userEmail
+        }
+      });
+      console.log('User retrieved!')
+      return user
+    } catch (e) {
+      console.log('Oh no! something really bad happened x0 getuser(userId)', e)
+    }
+  }
+
+  static async getAllusers() {
+    try {
+      let users = await User.findAll();
+      console.log('Users retrieved!')
+      return users
+    } catch (e) {
+      console.log('Oh no! something really bad happened x0 getAllusers()', e)
+    }
+  }
+
+  static async deleteuser(userId) {
+    try {
+      await User.destroy({
+        where: {
+          id: userId
+        }
+      })
+      console.log('Users deleted!')
+      return 1
+    } catch (e) {
+      console.log('Oh no! something really bad happened xD deleteuser(userId)', e)
+    }
+  }
+
+  //static async deleteAllusers(user)
+
+}
+
+module.exports = UserService;
