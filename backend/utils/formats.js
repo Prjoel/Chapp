ae78a6a786cc66d529751e654324ca560a1eb82f
Joel Pereira <joel.pereira.996@gmail.com>
2021-01-19 16:05:37 -0300

    add getMethods Function

--- backend/utils/formats.js
+++ backend/utils/formats.js
@@ -13,6 +13,18 @@ function validateUser(user) {
 
   return validEmail && validPassword && validUsername
 }
+/**
+ *   Returns an array with the method properties of a given object.
+ * @param {*} obj 
+ */
+const getMethods = (obj) => {
+  let properties = new Set()
+  let currentObj = obj
+  do {
+    Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
+  } while ((currentObj = Object.getPrototypeOf(currentObj)))
+  return [...properties.keys()].filter(item => typeof obj[item] === 'function')
+}
 
 
 module.exports = { formatMsg, validateUser };
